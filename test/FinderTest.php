d183479842c5b0b61524fcc1867281e9233f05e7
rap2hpoutre <raphaelht@gmail.com>
2015-05-08 14:35:02 +0200

    Update tests

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -9,4 +9,10 @@ class FinderTest extends \PHPUnit_Framework_TestCase
         $text_finder = new Finder('bananna', ['apple','kiwi','banana','orange','banner']);
         $this->assertEquals('banana', $text_finder->first());
     }
+
+    public function testAll()
+    {
+        $text_finder = new Finder('bananna', ['apple','kiwi','Ü◘ö']);
+        $this->assertCount(3, $text_finder->all());
+    }
 }
