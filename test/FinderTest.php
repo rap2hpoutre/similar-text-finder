facdebf47110f323ba8d9a63bedf9ab77c47a05a
rap2hpoutre <raphaelht@gmail.com>
2015-05-08 14:37:19 +0200

    Doc

--- test/FinderTest.php
+++ test/FinderTest.php
@@ -2,14 +2,24 @@
 
 use \SimilarText\Finder;
 
+/**
+ * Class FinderTest
+ * @package SimilarText\Test
+ */
 class FinderTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Test first() method
+     */
     public function testFirst()
     {
         $text_finder = new Finder('bananna', ['apple','kiwi','banana','orange','banner']);
         $this->assertEquals('banana', $text_finder->first());
     }
 
+    /**
+     * Test all() method
+     */
     public function testAll()
     {
         $text_finder = new Finder('bananna', ['apple','kiwi','Ü◘ö']);
